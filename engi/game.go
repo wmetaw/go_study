8cb902ea9b1c79c0db4525e42737e745291ea024
takashima <takashima@cloverlab.jp>
2015-07-30 14:01:09 +0900

    キャラアニメーション

--- engi/game.go
+++ engi/game.go
@@ -6,16 +6,27 @@ import (
 // "reflect"
 )
 
-var gravity float32 = 25
 var windowHeight float32 = 800
 var windowWidth  float32 = 1200
 
+// 重力
+const GRAVITY float32 = 5
+
+// アニメーション定数
+const (
+    ANIME_TYPE_WAIT = iota
+    ANIME_TYPE_RUN
+    ANIME_TYPE_JUMP
+    ANIME_TYPE_SHOOT
+)
+
+var JUMP_COUNTER = [...]float32 {-110,-90,-60,-30,-20,-10,-5,-3,-1,0,1,3,10,20,30,40,50,70}
 
 // ゲームオブジェクト
 type Game struct {
     *engi.Game
 
-    // ゴーファーくん
+    // ゴーファーくん構造体
     PL
 
     // 描画用バッチ？
@@ -32,42 +43,85 @@ type Game struct {
 type PL struct {
 
     img engi.Drawable
-    animeNo int
     posX float32
     posY float32
-    waitFrame float32
-    waitImg [2]engi.Drawable
-    // isJump bool = false
+
+    // アニメーション
+    animeNo int
+    animeType int
+    animeFrame float32
+    waitImages  [2]engi.Drawable // 待機モーション
+    runImages   [3]engi.Drawable // 移動モーション
+    jumpImages  [2]engi.Drawable // ジャンプモーション
+    shootImages [3]engi.Drawable // 発砲モーション
+
+    isJump bool
+    jumpCnt int
 }
 
 func (pl PL) Update(game *Game, dt float32) {
 
+    // 慣性ジャンプ
+    if game.PL.isJump && game.PL.jumpCnt < len(JUMP_COUNTER) {
+        game.posY += JUMP_COUNTER[game.PL.jumpCnt]
+        game.PL.jumpCnt++
+    }
+
     // 落下
-    game.PL.posY += gravity
+    game.PL.posY += GRAVITY
     if game.PL.posY > windowHeight - game.PL.img.Height()/4 {
         game.PL.posY = windowHeight - game.PL.img.Height()/4
+        game.PL.jumpCnt = 0
+        game.PL.isJump = false
     }
 
     // 5フレーム後にアニメーションを切り替え
     var isChange bool
-    if game.PL.waitFrame >= (dt*5) {
-        game.PL.waitFrame = dt
+    if game.PL.animeFrame >= (dt*5) {
+        game.PL.animeFrame = dt
         isChange = true
     }  else {
-        game.PL.waitFrame += dt
+        game.PL.animeFrame += dt
     }
 
     // アニメーション切り替え
     if isChange {
+        switch game.PL.animeType {
 
-        // キャラ画像を置換
-        game.PL.img = game.PL.waitImg[game.PL.animeNo]
+        // 待機モーション
+        case ANIME_TYPE_WAIT:
 
-        // アニメーション番号をカウントアップ
-        game.PL.animeNo++
-        if len(game.PL.waitImg) == game.PL.animeNo {
-            game.PL.animeNo = 0
+            // 画像の要素数を超えたらアニメ番号を0に戻す
+            if game.PL.animeNo >= len(game.PL.waitImages) {
+                game.PL.animeNo = 0
+            }
+
+            // キャラ画像を置換
+            game.PL.img = game.PL.waitImages[game.PL.animeNo]
+
+        // 走るモーション
+        case ANIME_TYPE_RUN:
+            if game.PL.animeNo >= len(game.PL.runImages) {
+                game.PL.animeNo = 0
+            }
+            game.PL.img = game.PL.runImages[game.PL.animeNo]
+
+        // 発砲モーション
+        case ANIME_TYPE_SHOOT:
+            if game.PL.animeNo >= len(game.PL.shootImages) {
+                game.PL.animeNo = 0
+            }
+            game.PL.img = game.PL.shootImages[game.PL.animeNo]
+
+        // ジャンプモーション
+        case ANIME_TYPE_JUMP:
+            if game.PL.animeNo > 0 {
+                game.PL.animeNo = 1
+            }
+            game.PL.img = game.PL.jumpImages[game.PL.animeNo]
         }
+
+        game.PL.animeNo++
     }
 
 }
@@ -77,7 +131,7 @@ func (pl PL) Update(game *Game, dt float32) {
  *
  * engiが描画ループしてくれるっぽい
  */
- func (game *Game) Render() {
+func (game *Game) Render() {
 
     game.batch.Begin()
 
@@ -96,35 +150,37 @@ func (pl PL) Update(game *Game, dt float32) {
  *
  * 【引数】時間差 float32(0.01635106とか）
  */
-    func (game *Game) Update(dt float32) {
+func (game *Game) Update(dt float32) {
 
-        var dx, dy float32
-        if game.keyMap[engi.ArrowUp] {
-            dy = -30
-        }
-        if game.keyMap[engi.ArrowDown] {
-            dy = 30
-        }
-        if game.keyMap[engi.ArrowLeft] {
-            dx = -10
-        }
-        if game.keyMap[engi.ArrowRight] {
-            dx = 10
-        }
-        if game.keyMap[engi.Space] {
-            game.PL.posY -= 30
-        }
-
-        game.PL.posX += dx
-        game.PL.posY += dy
-
-        game.PL.Update(game, dt)
+    var dx float32
+    game.PL.animeType = ANIME_TYPE_WAIT
+    if game.keyMap[engi.ArrowLeft] {
+        dx = -10
+        game.PL.animeType = ANIME_TYPE_RUN
     }
+    if game.keyMap[engi.ArrowRight] {
+        dx = 10
+        game.PL.animeType = ANIME_TYPE_RUN
+    }
+    if game.keyMap[engi.S] {
+        game.PL.animeType = ANIME_TYPE_SHOOT
+    }
+    if game.keyMap[engi.Space] {
+        game.PL.isJump = true
+    }
+    if game.PL.isJump {
+        game.PL.animeType = ANIME_TYPE_JUMP
+    }
+
+    game.PL.posX += dx
+
+    game.PL.Update(game, dt)
+}
 
 /**
  * キー入力判定
  */
- func (game *Game) Key(key engi.Key, modifier engi.Modifier, action engi.Action) {
+func (game *Game) Key(key engi.Key, modifier engi.Modifier, action engi.Action) {
 
     // 対応するキーのフラグをON
     switch action {
@@ -151,11 +207,23 @@ func (pl PL) Update(game *Game, dt float32) {
  *
  * 最初に呼び出される
  */
- func (game *Game) Preload() {
+func (game *Game) Preload() {
 
     // spriteの読み込み
+    engi.Files.Add("plWait0", "img/player/wait/0.png")
     engi.Files.Add("plWait1", "img/player/wait/1.png")
-    engi.Files.Add("plWait2", "img/player/wait/2.png")
+
+    engi.Files.Add("plRun0", "img/player/run/0.png")
+    engi.Files.Add("plRun1", "img/player/run/1.png")
+    engi.Files.Add("plRun2", "img/player/run/2.png")
+
+    engi.Files.Add("plShoot0", "img/player/shoot/0.png")
+    engi.Files.Add("plShoot1", "img/player/shoot/1.png")
+    engi.Files.Add("plShoot2", "img/player/shoot/2.png")
+
+    engi.Files.Add("plJump0", "img/player/jump/0.png")
+    engi.Files.Add("plJump1", "img/player/jump/1.png")
+
     engi.Files.Add("font", "img/font.png")
 
     game.batch = engi.NewBatch(engi.Width(), engi.Height())
@@ -165,8 +233,10 @@ func (pl PL) Update(game *Game, dt float32) {
 
     // プレイヤーキャラ初期化
     game.PL.posX, game.PL.posY = 512, 320
-    game.PL.waitFrame = 0
+    game.PL.animeFrame = 0
     game.PL.animeNo = 0
+    game.PL.animeType = ANIME_TYPE_WAIT
+    game.PL.jumpCnt = 0;
 }
 
 /**
@@ -174,13 +244,25 @@ func (pl PL) Update(game *Game, dt float32) {
  *
  * Preloadの次に呼ばれる
  */
- func (game *Game) Setup() {
+func (game *Game) Setup() {
 
     // 背景
     engi.SetBg(0xfffffa)
-    game.PL.waitImg[0] = engi.Files.Image("plWait1")
-    game.PL.waitImg[1] = engi.Files.Image("plWait2")
-    game.PL.img = game.PL.waitImg[0]
+    game.PL.waitImages[0] = engi.Files.Image("plWait0")
+    game.PL.waitImages[1] = engi.Files.Image("plWait1")
+
+    game.PL.runImages[0]  = engi.Files.Image("plRun0")
+    game.PL.runImages[1]  = engi.Files.Image("plRun1")
+    game.PL.runImages[2]  = engi.Files.Image("plRun2")
+
+    game.PL.shootImages[0]  = engi.Files.Image("plShoot0")
+    game.PL.shootImages[1]  = engi.Files.Image("plShoot1")
+    game.PL.shootImages[2]  = engi.Files.Image("plShoot2")
+
+    game.PL.jumpImages[0] = engi.Files.Image("plJump0")
+    game.PL.jumpImages[1] = engi.Files.Image("plJump1")
+
+    game.PL.img = game.PL.waitImages[0]
     game.font = engi.NewGridFont(engi.Files.Image("font"), 20, 20)
 }
 
